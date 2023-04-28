194cd005f35e5c1d0b628a78e19219cfec571140
thainq168 <19522189@gm.uit.edu.vn>
2023-04-28 21:36:14 +0700

    Add Entity

---
+++ eShopSolution.Data/Entities/Language.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Language
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
---
+++ eShopSolution.Data/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Guid UserId { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipEmail { get; set; }
+        public string ShipPhoneNumber { get; set; }
+        public OrderStatus Status { get; set; }
+
+    }
+}
